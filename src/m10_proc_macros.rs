7dff45611eeb945b192f4264e5f5ab02b338f199
Rupa Netha <iamsairupa06@gmail.com>
2025-07-09 20:05:46 +0530

    add ai llm and test function

--- src/m10_proc_macros.rs
+++ src/m10_proc_macros.rs
@@ -13,4 +13,20 @@ mod test {
     println!("{}", _OUTPUT);
   }
 
+  #[function_to_string]
+  fn some_function_for_ai_llm(_whatever_param: &str) {
+    /// This is an awesome function
+    /// We shall give it to an AI to guess and output
+    /// In a structured manner
+    println!("{}", _OUTPUT);
+  }
+
+  #[test]
+  fn tests_proc_macro() {
+    
+    let x: &str = some_function_for_ai_llm("some_input");
+    let y: &str = another_ai_function("some_input_again");
+    dbg!(x);
+    dbg!(y);
+  }
 }
